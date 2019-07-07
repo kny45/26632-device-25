f682dbce3b66a2b1216537b7a2230d3d450ac146
kny45 <kny.komarov@gmail.com>
2019-07-07 21:08:39 +0300

    Update script.js
    Добавлеа обработка ошибки заполнения полей на форме обратной связи

--- js/script.js
+++ js/script.js
@@ -1,61 +1,62 @@
 
-    var link = document.querySelector(".contacts__btn-write-to-us");
-    var popup = document.querySelector(".feedback-form");
-    var close = popup.querySelector(".feedback-form__close");
-    var form = popup.querySelector("form");
-    var name = popup.querySelector("[name=feedback-form__input-name]");
-    var email = popup.querySelector("[name=feedback-form__input-mail]");
+var link = document.querySelector(".contacts__btn-write-to-us");
+var popup = document.querySelector(".feedback-form");
+var close = popup.querySelector(".feedback-form__close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=feedback-form__input-name]");
+var email = popup.querySelector("[name=feedback-form__input-mail]");
 
-    var mapLink = document.querySelector(".contact__map-link-wrapper");
-    var mapPopup = document.querySelector(".map-form");
-    var mapClose = document.querySelector(".map-form__close");
+var mapLink = document.querySelector(".contact__map-link-wrapper");
+var mapPopup = document.querySelector(".map-form");
+var mapClose = document.querySelector(".map-form__close");
 
-    link.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popup.classList.add("feedback-form-show");
-    });
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("feedback-form-show");
+});
 
-    close.addEventListener("click", function (evt) {
-      evt.preventDefault();
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("feedback-form-show");
+  popup.classList.remove("feedback-form-error");
+});
+
+popup.addEventListener("submit", function(evt) {
+  popup.classList.remove("feedback-form-error");
+
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("feedback-form-error");
+    console.log("Введите имя и email");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("feedback-form-show")) {
       popup.classList.remove("feedback-form-show");
       popup.classList.remove("feedback-form-error");
-    });
+    }
+  }
+});
 
-    popup.addEventListener("submit", function(evt) {
-      if (!name.value || !email.value) {
-        evt.preventDefault();
-        popup.classList.add("feedback-form-error");
-        /*
-        if (!name.value) {
-          name.classList.add("feedback-form__input--error");
-        };
-        if (!email.value) {
-          email.classList.add("feedback-form__input--error");
-        };
-        */
-        console.log("Введите имя и email");
-      }
-    });
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("map-form-show");
+});
 
-    window.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popup.classList.contains("feedback-form-show")) {
-          popup.classList.remove("feedback-form-show");
-          popup.classList.remove("feedback-form-error");
-        }
-      }
-    });
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("map-form-show");
+});
 
-
-    mapLink.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      mapPopup.classList.add("map-form-show");
-    });
-
-    mapClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("map-form-show")) {
       mapPopup.classList.remove("map-form-show");
-    });
-
-
+    }
+  }
+});
